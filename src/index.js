fee54c519b4c11909f6bbd89cf8b1137abaf5cb1
aparnamalladi18 <aparnamalladi18@gmail.com>
2024-05-03 06:58:00 +0530

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
 // MEME GENERATOR APP 
 
-// import React from "react"
-// import ReactDOM from "react-dom"
-// import App from "./App"
-// import  './style.css' 
-// ReactDOM.render(<App />, document.getElementById("root")) 
+import React from "react"
+import ReactDOM from "react-dom"
+import App from "./App"
+import  './style.css' 
+ReactDOM.render(<App />, document.getElementById("root")) 
 
 
 // MARKDOWN EDITOR APP 
-import ReactMde from "react-mde";
-import 'react-mde/lib/styles/css/react-mde-all.css'; 
+// import ReactMde from "react-mde";
+// import 'react-mde/lib/styles/css/react-mde-all.css'; 
 
-import React from "react"
-import ReactDOM from "react-dom"
+// import React from "react"
+// import ReactDOM from "react-dom"
 
-import App from "./App"
-ReactDOM.render(<App />, document.getElementById("root"))
+// import App from "./App"
+// ReactDOM.render(<App />, document.getElementById("root")) 
