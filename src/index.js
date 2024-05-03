b72f5f417cb2a2c857bc53e9efa036de9ee11404
aparnamalladi18 <aparnamalladi18@gmail.com>
2024-05-03 06:23:19 +0530

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// MEME GENERATOR APP 
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// import React from "react"
+// import ReactDOM from "react-dom"
+// import App from "./App"
+// import  './style.css' 
+// ReactDOM.render(<App />, document.getElementById("root")) 
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+// MARKDOWN EDITOR APP 
+import ReactMde from "react-mde";
+import 'react-mde/lib/styles/css/react-mde-all.css'; 
+
+import React from "react"
+import ReactDOM from "react-dom"
+
+import App from "./App"
+ReactDOM.render(<App />, document.getElementById("root"))
